d84a1b684128daf87cf8f55adaa615faea2b9fe0
IsaacGHardy <isaacghardy@gmail.com>
2025-07-24 16:42:26 -0500

    Update to be usable by frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.players import router as players_router
 
 app = FastAPI(title="NBA Fantasy API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(players_router)
 
